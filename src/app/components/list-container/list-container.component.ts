8cbf9d9de2e957eec5395b1ff84d9f5e82c90a4b
Julio Cuneo <jucuneo@gmail.com>
2023-01-22 22:55:45 -0300

    unsubscribe implemented

--- src/app/components/list-container/list-container.component.ts
+++ src/app/components/list-container/list-container.component.ts
@@ -1,24 +1,28 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {Activity} from "../../interfaces/Activities";
 import {CalendarService} from "../../calendar.service";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-list-container',
   templateUrl: './list-container.component.html',
   styleUrls: ['./list-container.component.css']
 })
-export class ListContainerComponent implements OnInit {
+export class ListContainerComponent implements OnInit, OnDestroy {
   @Input() title: string = '';
   @Input() date: Date | null = null
   @Output() drop = new EventEmitter<any>();
 
   activities: Activity[] = []
+  notifier = new Subject()
 
   constructor(public calendarService: CalendarService) {
   }
 
   ngOnInit() {
-    this.calendarService.activitiesSubject.subscribe(activities =>
+    this.calendarService.activitiesSubject
+      .pipe(takeUntil(this.notifier))
+      .subscribe(activities =>
       this.activities = this.calendarService.filterActivitiesByDate(
         this.date?.toISOString().split('T')[0],
         activities
@@ -26,9 +30,14 @@ export class ListContainerComponent implements OnInit {
     )
   }
 
-
   onDrop(e: any) {
     this.drop.emit(e);
+  }
+
+  ngOnDestroy() {
+    // @ts-ignore
+    this.notifier.next()
+    this.notifier.complete()
   }
 
 }
